6d006e22144b625558e9289c4b71e584e3f3612e
Karlos <karin_ar72@hotmail.com>
2022-12-25 20:25:08 -0600

    addition and subtraction working

--- src/App.js
+++ src/App.js
@@ -109,8 +109,59 @@ while(inputArray.indexOf('x') != -1) {
 multiplication();
 }
 
+function addOrSub() {
+//Search an adition or subtraction
+const sumOrMinusIndex = inputArray.findIndex((elem) => elem === '+' || elem === '-');
+//Get the right side of the operation
+//!Bug: If the ecuation begins with '+' or '-' the algorithm gonna take the numbers next the sign as the right side of the expression
+if(sumOrMinusIndex != -1) {
+  const signsToRight = inputArray.findIndex((n, index) => (n === 'x' || n === '+' || n === '-' || n === '/') && index > sumOrMinusIndex);
+  let right;
+  if(signsToRight != -1) {
+  const rightSide = inputArray.slice(sumOrMinusIndex + 1, signsToRight);
+  right = rightSide;
 
-
+  } else {
+    const rightSide = inputArray.slice(sumOrMinusIndex + 1);
+    right = rightSide;
+    
+  }
+  //Get the left side of the operation
+  //Find an operator on the left
+  let left;
+  let startFragmentIndex;
+  for(let i = sumOrMinusIndex - 1; i >= 0 ; i--) {
+    const element = inputArray[i];
+    if(element === '/' || element === '+' || element === '-' || element === 'x') {
+      const leftSide = inputArray.slice(i + 1, sumOrMinusIndex);
+      left = leftSide;
+      startFragmentIndex = i + 1;
+      break;
+    } else {
+      const leftSide = inputArray.slice(0, sumOrMinusIndex);
+      left = leftSide;
+      startFragmentIndex = 0;
+    }
+  }
+  //Excecute left and right and embeed the result
+  let leftNumber = Number(left.join(''));
+  let rightNumber = Number(right.join(''));
+  let total;
+  if(inputArray[sumOrMinusIndex] === '+') {
+    let result = leftNumber + rightNumber;
+    total = result;
+  } else {
+    let result = leftNumber - rightNumber;  
+    total = result;  
+  }
+  inputArray.splice(startFragmentIndex, right.length + left.length + 1, total);
+  return inputArray;
+  }
+}
+while(inputArray.findIndex((elem) => elem === '+' || elem === '-') != -1) {
+  addOrSub();
+  }
+  
 {/*
   -There is a division sign?
     Yes:
@@ -149,6 +200,19 @@ multiplication();
     Yes: 
       Repeat the previous steps.
     No: 
+    -There is an addition or a subtraction sign?
+       Yes:
+      -There is an operator on the right?
+        Yes:
+          -Slice from the addition or a subtraction operator + 1 to the next operator
+        No: 
+          -Slice from the addition or a subtraction operator + 1 omitting the end.
+      -There is an operator on the left?
+        Yes:
+         -Slice from the left operator + 1 to the addition or a subtraction operator.
+        No:
+         -Slice from start until the addition or a subtraction operator.
+      -Become left and right to type number and asign a variable to store left + or - right.
 
   
 */}
